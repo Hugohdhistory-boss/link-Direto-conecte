837acd06d7530c4b9307d4cb64cfd5333d312a29
Hugohdhistory-boss <historyfilme@gmail.com>
2026-08-29 18:03:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='link-direto-v5-capa';
-const ASSETS=['./','./index.html','./styles.css','./cover-v5.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png','./capa-link-direto-v5.png'];
+const CACHE_NAME='link-direto-v6-interior';
+const ASSETS=['./','./index.html','./styles.css','./cover-v5.css','./v6-enhancements.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png','./capa-link-direto-v5.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
